040efffc7904bf981f8dd69d01f8b0b6956bce04
Wamaitha Nyamu <wamaithanyamu@gmail.com>
2020-08-05 11:53:17 +0300

    Signup and signin

---
+++ app/src/main/java/com/example/blood_donation_app/donorLogin.java
@@ -0,0 +1,14 @@
+package com.example.blood_donation_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class donorLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donor_login);
+    }
+}
