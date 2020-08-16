5dc21a3d780a48b398e66934290ff47cbdb66f0c
Wamaitha Nyamu <wamaithanyamu@gmail.com>
2020-08-16 15:00:10 +0300

    Showing user logged in on dashboard

--- app/src/main/java/com/example/blood_donation_app/hospitalDashboard.java
+++ app/src/main/java/com/example/blood_donation_app/hospitalDashboard.java
@@ -4,16 +4,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import androidx.viewpager.widget.ViewPager;
 
 
 public class hospitalDashboard extends AppCompatActivity {
     private Button addDonor, editProfile, recentDonors, hospitalBloodBank;
+    private TextView hospitalName;
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
+    DatabaseReference child;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +36,26 @@ public class hospitalDashboard extends AppCompatActivity {
         editProfile = findViewById(R.id.editHospital);
         recentDonors = findViewById(R.id.recentDonors);
         hospitalBloodBank = findViewById(R.id.hospitalBloodBank);
+        hospitalName = findViewById(R.id.loggedInHospitalName);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String hospitalUID = user.getUid();
+        rootNode = FirebaseDatabase.getInstance();
+        reference = rootNode.getReference("Hospitals").child(hospitalUID);
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String name = (String) dataSnapshot.child("newHospitalName").getValue();
+                hospitalName.setText(name);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
 
         addDonor.setOnClickListener(new View.OnClickListener() {
             @Override
